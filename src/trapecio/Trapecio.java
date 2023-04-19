1f4d73ce28cd71500815efeda4be2c7f3eb6a183
Rodrigo Ojeda Arce <rojedaa@ulasalle.edu.pe>
2023-04-19 14:04:57 -0500

    Creando el inicio del programa

--- src/trapecio/Trapecio.java
+++ src/trapecio/Trapecio.java
@@ -1,5 +1,17 @@
+/*
+ * Class                :Trapecio.java
+ * Description          :Calcular el metodo del trapecio con programacion paralela y secuencial
+ * Author               :Rodrigo Ojeda Arce
+ * Course               :Lenguaje a la programacion 3
+ * Editor               :Eclipse
+ */
 package trapecio;
-
+	/*
+	 * Input 01         :Funcion Evaluar. Tipo: String. Ejemplo: "2x^2+1"
+	 * input 02         :Limite Inferior. Tipo: int. Ejemplo: 5
+	 * Input 03         :Limite superior. Tipo: in. Ejemplo: 10
+	 */
 public class Trapecio {
 
+	long start = System.nanoTime();
 }
