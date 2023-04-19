d20e29dae1f115c026a436de805553a5051fb907
Rodrigo Ojeda Arce <rojedaa@ulasalle.edu.pe>
2023-04-19 14:48:41 -0500

    Se agrego el nanotime para poder medir el tiempo en las funciones

--- src/trapecio/Trapecio.java
+++ src/trapecio/Trapecio.java
@@ -11,7 +11,24 @@ package trapecio;
 	 * input 02         :Limite Inferior. Tipo: int. Ejemplo: 5
 	 * Input 03         :Limite superior. Tipo: in. Ejemplo: 10
 	 */
-public class Trapecio {
 
-	long start = System.nanoTime();
+
+public class Trapecio {
+	
+	public static void main(String args[]) {
+
+		long start = System.nanoTime();
+	
+		System.out.println(" - - - - - - - - - - - -");
+		System.out.println(" - Método del trapecio -");
+		System.out.println(" - - - - - - - - - - - -");
+		System.out.println("Función a evaluar : " + args[0]);
+		System.out.println("Límite inferior : " + args[1]);
+		System.out.println("Límite superior : " + args[2]);
+	
+		long end = System.nanoTime();
+	
+		//Mostrando el tiempo total de la ejecución del programa
+		System.out.println("\n" + "TIEMPO TOTAL DE EJECUCIÓN : " + (end-start) + " ns.");
+  }
 }
