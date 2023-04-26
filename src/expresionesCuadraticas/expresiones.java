326e5f25c2a179d66e7807d7589716a472f785eb
Rodrigo Ojeda Arce <rojedaa@ulasalle.edu.pe>
2023-04-25 22:01:10 -0500

    Se creo una expresion para que el usuario digite los limites superiores e inferiores

--- src/expresionesCuadraticas/expresiones.java
+++ src/expresionesCuadraticas/expresiones.java
@@ -1,5 +1,30 @@
 package expresionesCuadraticas;
 
+import java.util.*;
+//import net.objecthunter.exp4j.*;
+
 public class expresiones {
 
+	public static void main (String[] args) {
+		String ingreso;
+		double limite_superior, limite_inferior;
+		double resultado;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Ingrese una funcion");
+		
+		ingreso = input.nextLine();
+		
+		Expresion e = new ExpresionConstructor("x^2+2");
+				.variable("x");
+				.build();
+				
+		System.out.println("Digite el limite superior");
+		limite_superior = input.nextDouble();
+		
+		System.out.println("Digite el limite inferor");
+		limite_inferior = input.nextDouble();
+		
+		
+	}
 }
