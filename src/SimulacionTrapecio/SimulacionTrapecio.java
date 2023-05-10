6461785982482b56f0ac66536c2ba02cf3ca9414
Rodrigo Ojeda Arce <rojedaa@ulasalle.edu.pe>
2023-05-09 21:57:39 -0500

    Se agrego expp4j para que se pueda compilar por consola y ademas se hizo la simulacion 1 logrando ejecutarse la programacion paralela, en la cual se creo un objeto trapecio1 el cual se inicializa y hace los calculos correctamente

--- src/SimulacionTrapecio/SimulacionTrapecio.java
+++ src/SimulacionTrapecio/SimulacionTrapecio.java
@@ -1,5 +1,21 @@
 package SimulacionTrapecio;
 
-public class SimulacionTrapecio {
 
+public class SimulacionTrapecio {
+	
+	private TrapecioParalelo trapecio1;
+	
+	public void initialize() {
+		trapecio1 = new TrapecioParalelo();
+		trapecio1.start();
+		}
+	
+	
+	SimulacionTrapecio(){
+		initialize();
+	}
+	public static void main (String[] args) {
+		SimulacionTrapecio window = new SimulacionTrapecio();
+		
+	}
 }
